4b156d2e8d8027a34a1cc80aed3fefeefb2a1774
Geeth <induwarakwg3573@gmail.com>
2023-12-09 22:21:56 +0530

    Refactor App component

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { Button, Container, Grid, Typography } from "@mui/material";
+import InputAmout from "./components/InputAmout";
+import SelectCountry from "./components/SelectCountry";
+import SwitchCurrency from "./components/SwitchCurrency";
+
+function App() {
+  return (
+    <Container maxWidth="md" sx={{ background: "#fdfdfd" }}>
+      <Typography variant="h5" sx={{ marginTop: "2rem" }}>
+        Stay Ahead With Accurate Conversion
+      </Typography>
+      <Grid container spacing={2}>
+        <InputAmout />
+        <SelectCountry />
+        <SwitchCurrency />
+        <SelectCountry />
+      </Grid>
+    </Container>
+  );
+}
+
+export default App;
