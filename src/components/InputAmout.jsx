de87c0040f7222737120e25a41feffce28746b6b
Geeth <induwarakwg3573@gmail.com>
2023-12-09 23:03:18 +0530

    Update InputAmout component to include TextField for amount input

--- src/components/InputAmout.jsx
+++ src/components/InputAmout.jsx
@@ -1,6 +1,16 @@
-import { Button, Container, Grid, Typography } from "@mui/material";
+import { Button, Container, Grid, TextField, Typography } from "@mui/material";
 const InputAmout = () => {
-  return <Grid item> Item </Grid>;
+  return (
+    <Grid item>
+      <TextField
+        label="Amount"
+        fullWidth
+        InputProps={{
+          type: "number",
+        }}
+      />
+    </Grid>
+  );
 };
 
 export default InputAmout;
