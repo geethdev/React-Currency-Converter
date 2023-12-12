604f61afa56e5abc0a73b4733a6b7bc9edbbf44a
Geeth <induwarakwg3573@gmail.com>
2023-12-12 21:20:57 +0530

    Update typography and input fields

--- src/components/InputAmout.jsx
+++ src/components/InputAmout.jsx
@@ -1,12 +1,20 @@
-import { Button, Container, Grid, TextField, Typography } from "@mui/material";
+import {
+  Button,
+  Container,
+  Grid,
+  InputAdornment,
+  TextField,
+  Typography,
+} from "@mui/material";
 const InputAmout = () => {
   return (
-    <Grid item>
+    <Grid item xs={12}>
       <TextField
         label="Amount"
         fullWidth
         InputProps={{
           type: "number",
+          startAdornment: <InputAdornment position="start">$</InputAdornment>,
         }}
       />
     </Grid>
