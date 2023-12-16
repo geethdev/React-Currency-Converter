f1419541bcd414cf1938de5ba4f6a5272a51af98
Geeth <induwarakwg3573@gmail.com>
2023-12-16 22:01:56 +0530

    Add axios and update styles

--- src/components/SelectCountry.jsx
+++ src/components/SelectCountry.jsx
@@ -1,11 +1,21 @@
 import { Autocomplete, Grid, TextField } from "@mui/material";
+import useAxios from "../hooks/useAxios";
 
 const SelectCountry = () => {
+  const [data] = useAxios("https://restcountries.com/v3.1/all");
+
+  const dataFilter = data.filter((item) => "currencies" in item);
+  const dataCountries = dataFilter.map((item) => {
+    return `${item.flag} ${Object.keys(item.currencies)[0]} - ${
+      item.name.common
+    }`;
+  });
+
   return (
     <Grid item xs={12} md={3}>
       <Autocomplete
         value="option1"
-        options={["option1", "option2", "option3"]}
+        options={dataCountries}
         renderInput={(params) => <TextField {...params} label="from" />}
       />
     </Grid>
